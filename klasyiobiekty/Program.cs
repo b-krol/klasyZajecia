0de1e0bf52afcbfb5c76abfdf2617b8c17c6d8d8
krol.bartosz <krol.bartosz@STACJA105-09>
2023-01-13 08:15:07 +0100

    Dodaj pliki projektów.

---
+++ klasyiobiekty/Program.cs
@@ -0,0 +1,15 @@
+﻿using klasyiobiekty.przykladyklas;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Osoba cz1 = new Osoba();
+        cz1.imie = "Kacper";
+        Console.WriteLine(cz1.imie + " to twoje imie");
+        
+
+
+    }
+}
---
+++ klasyiobiekty/przykladyklas/osoba.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasyiobiekty.przykladyklas
+{
+    internal class Osoba
+    {
+        private string imie, nazwisko;
+        private DateTime dtUrodzenia;
+    }
+}
